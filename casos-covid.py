ae56bd2b2c82d8548dfce1428da6410e8c32cbdd
jesus diaz <jesusdiazg9@gmail.com>
2022-06-01 13:03:32 -0500

    segundo commit

--- casos-covid.py
+++ casos-covid.py
@@ -11,4 +11,33 @@ data.drop('Nombre del país', axis = 1, inplace=True)
 data.drop('Pertenencia étnica', axis = 1, inplace=True)
 data.drop('Nombre del grupo étnico', axis = 1, inplace=True)
 data.drop('Fecha de inicio de síntomas', axis = 1, inplace=True)
+data.drop('Unidad de medida de edad', axis = 1, inplace=True)
+data.drop('Código DIVIPOLA departamento', axis = 1, inplace=True)
+data.drop('Código DIVIPOLA municipio', axis = 1, inplace=True)
+data.drop('ID de caso', axis = 1, inplace=True)
+
+# Normalizar  Estado
+
+data.loc[data['Estado'] == 'leve'] = 'Leve'
+data.loc[data['Estado'] == 'LEVE'] = 'Leve'
+
+# Normalizar  sexo
+
+data.loc[data['Sexo'] == 'm'] = 'M'
+data.loc[data['Sexo'] == 'f'] = 'F'
+
+# Normalizar  Estado
+
+data.loc[data['Estado'] == 'M'] = 'Moderado'
+data.loc[data['Sexo'] == 'F'] = 'Fallecido'
+
+# Normalizar  Ubicacion del caso
+data.loc[data['Ubicación del caso'] == 'casa'] = 'Casa'
+data.loc[data['Ubicación del caso'] == 'CASA'] = 'Casa'
+
+# Normalizar  Recuperado
+data.loc[data['Recuperado'] == 'fallecido'] = 'Fallecido'
+
+
+#print(data.columns)
 
